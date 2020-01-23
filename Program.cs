5033d353144654cd02045cb8db5448c4dc141f4a
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-01-23 09:43:51 -0500

    Added functionality for input validation and removed code execution from the Main method by creating the BMI_Run() method

--- Program.cs
+++ Program.cs
@@ -10,20 +10,48 @@ namespace BMI_Calculator
     {
         static void Main(string[] args)
         {
-            //requests user input for height and weight
-            Console.Write("Please input height: ");
-            int height = int.Parse(Console.ReadLine());
-            Console.Write("Please input weight: ");
-            int weight = int.Parse(Console.ReadLine());
-            //pass user input to BMI() and WL BMI()'s return string
-            Console.WriteLine(BMI(height, weight));
+            BMI_Run();
+        }
+
+        static void BMI_Run()
+        {
+            double height, weight;
+            int age;
+
+            //requests user input for height and validates that the input is parsed to a double
+            Console.Write("Please input height in inches: ");
+            double.TryParse(Console.ReadLine(), out height);
+            while (height == 0)
+            {
+                Console.Write("Error! Please enter a number of inches: ");
+                double.TryParse(Console.ReadLine(), out height);
+            }
+            //requests user input for weight and validates that the input is parsed to a double
+            Console.Write("Please input weight in pounds: ");
+            double.TryParse(Console.ReadLine(), out weight);
+            while (weight == 0)
+            {
+                Console.Write("Error! Please enter a number of pounds: ");
+                double.TryParse(Console.ReadLine(), out weight);
+            }
+            //requests user input for age and validates that the input is parsed to an int
+            Console.Write("Please input age in years: ");
+            int.TryParse(Console.ReadLine(), out age);
+            while (age == 0)
+            {
+                Console.Write("Error! Please enter a number of years: ");
+                int.TryParse(Console.ReadLine(), out age);
+            }
+            //pass user input to BMI() and output BMI()'s return string if age input >= 18
+            if (age >= 18) Console.WriteLine(BMI(height, weight));
+            else Console.WriteLine("User not above age 18.");
             Console.ReadKey();
         }
 
-        static string BMI(double height, double weight)
+        static string BMI(double h, double w)
         {
             //calculate BMI using height and weight
-            double bmi = (weight * 703) / (height * height);
+            double bmi = (w * 703) / (h * h);
             //if/else series uses BMI to return a string based on the range in which BMI falls
             if (bmi < 16) return $"Your BMI is {bmi}. You must gain weight.";
             else if (bmi < 18.5) return $"Your BMI is {bmi}. You should gain weight.";
