966ac521bde385da2ce2e751df554735ec3845a0
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-01-23 08:08:20 -0500

    Add static string method BMI() and add functionality.  Add code to Main() to call BMI and require user input.

--- Program.cs
+++ Program.cs
@@ -10,7 +10,38 @@ namespace BMI_Calculator
     {
         static void Main(string[] args)
         {
+            Console.Write("Please input height: ");
+            int height = int.Parse(Console.ReadLine());
+            Console.Write("Please input weight: ");
+            int weight = int.Parse(Console.ReadLine());
+            Console.WriteLine(BMI(height, weight));
+            Console.ReadKey();
+        }
 
+        static string BMI(double height, double weight)
+        {
+            double bmi = (weight * 703) / (height * height);
+            if (bmi < 16)
+            {
+                return $"Your BMI is {bmi}. You must gain weight.";
+            }
+            else if (bmi <= 16 && bmi > 18.5)
+            {
+                return $"Your BMI is {bmi}. You should gain weight.";
+            }
+            else if (bmi <= 18.5 && bmi > 25)
+            {
+                return $"Your BMI is {bmi}.  Maintain your weight.";
+            }
+            else if (bmi <= 25 && bmi > 30)
+            {
+                return $"Your BMI is {bmi}. You should lose weight.";
+            }
+            else if (bmi <= 30)
+            {
+                return $"Your BMI is {bmi}. You must lose weight.";
+            }
+            else return "Please input height and weight.";
         }
     }
 }
